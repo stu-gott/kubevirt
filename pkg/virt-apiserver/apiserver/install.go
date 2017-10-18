a5c6e2935d9f3ac0cc7a1f9687342be17dc27535
Stu Gott <sgott@redhat.com>
2017-10-18 11:16:47 -0400

    Register internal resource versions during metafactory install

--- pkg/virt-apiserver/apiserver/install.go
+++ pkg/virt-apiserver/apiserver/install.go
@@ -4,8 +4,8 @@ import (
 	"k8s.io/apimachinery/pkg/apimachinery/announced"
 	"k8s.io/apimachinery/pkg/apimachinery/registered"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/sets"
 
+	"kubevirt.io/kubevirt/pkg/api"
 	"kubevirt.io/kubevirt/pkg/api/v1"
 )
 
@@ -13,10 +13,11 @@ import (
 func Install(groupFactoryRegistry announced.APIGroupFactoryRegistry, registry *registered.APIRegistrationManager, scheme *runtime.Scheme) {
 	if err := announced.NewGroupMetaFactory(
 		&announced.GroupMetaFactoryArgs{
-			GroupName:                  v1.GroupName,
-			RootScopedKinds:            sets.NewString("VirtualMachine", "VirtualMachineList", "Migration", "MigrationList", "VirtualMachineReplicaSet", "VirtualMachineReplicaSetList"),
+			// NOTE: RootScopedKinds are not namespaced. don't use it for general resources
+			GroupName: v1.GroupName,
+			//RootScopedKinds:            sets.NewString("VirtualMachine", "VirtualMachineList", "Migration", "MigrationList", "VirtualMachineReplicaSet", "VirtualMachineReplicaSetList"),
 			VersionPreferenceOrder:     []string{v1.SchemeGroupVersion.Version},
-			AddInternalObjectsToScheme: v1.AddToScheme,
+			AddInternalObjectsToScheme: api.AddToScheme,
 		},
 		announced.VersionToSchemeFunc{
 			v1.SchemeGroupVersion.Version: v1.AddToScheme,
